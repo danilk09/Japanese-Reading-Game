0c5db9f3c548ba4a5721ce07d3df676c4f5f9c6f
indoorrockclimbing101 <juneplant2003@gmail.com>
2025-10-10 19:45:06 -1000

    Inserted hirigana data into sql database

--- storage/katakana_sql.c
+++ storage/katakana_sql.c
@@ -1,57 +1,153 @@
-#include <sqlite3.h>
 #include <stdio.h>
+#include <string.h>
+#include <sqlite3.h>
+
+int create_table(char [], sqlite3 *, char *, int);
+int insert_data(char [], char [], sqlite3 *, int);
 
 int main(int argc, char *argv[])
 {
-    char filename[20] = "katakana.db";
+    char db_name[20] = "katakana.db";
+    char filename[20] = "../katakana.txt";
+    char *error_messgae;
+    sqlite3 *db;
+    int ret_val = 0, exit = 0;
 
-    sqlite3* db;
-    int exit = 0;
+    if (create_table(db_name, db, error_messgae, exit) == -1)
+    {
+        printf("Error creating table\n");
+        ret_val = -1;
+    }
+    else
+    {
+        printf("Successfully created database table\n");
+    }
+
+    if (insert_data(db_name, filename, db, exit) == -1)
+    {
+        printf("Error inserting data\n");
+        ret_val = -1;
+    }
+    else
+    {
+        printf("Successfully inserted data\n");
+    }
+
+    return ret_val;
+}
+
+int create_table(char db_name[], sqlite3 *db, char *error_message, int exit)
+{
     char *sql = "CREATE TABLE katakana_mode ("
-                "ID INT PRIMARY KEY NOT NULL, "
+                "ID INTEGER PRIMARY KEY AUTOINCREMENT, "
                 "Katakana TEXT NOT NULL, "
-                "Romaji TEXT NOT NULL );";
-    char *errormessage;
+                "Romaji TEXT NOT NULL, "
+                "English TEXT NOT NULL );";
+    int ret_val = 0;
 
-    exit = sqlite3_open(filename, &db);
-    exit = sqlite3_exec(db, sql, NULL, 0, &errormessage);
+    exit = sqlite3_open(db_name, &db);
 
     if (exit)
     {
-        printf("Error opening database\n");
-        printf("%s\n", errormessage);
-        return -1;
+        ret_val = -1;
     }
-    else
+    
+    if (ret_val == 0)
     {
-        printf("Opened database\n");
+        exit = sqlite3_exec(db, sql, NULL, 0, &error_message);
+
+        if (exit)
+        {
+            printf("%s\n", error_message);
+            sqlite3_close(db);
+            ret_val = -1;
+        }
     }
 
-    sqlite3_close(db);
-    return 0;
+    if (ret_val == 0)
+    {
+        sqlite3_close(db);
+    }
+
+    return ret_val;
 }
 
-/*
-int i = 0;
-char word[]; // wchar ??
-while(feof == 0)
+int insert_data(char db_name[], char filename[], sqlite3 *db, int exit)
 {
-    fgets(word, 100, outfile);
+    char katakana[50];
+    char romaji[50];
+    char english[50];
+    int ret_val = 0;
 
-    // Do this or find a way where the sql statement takes a variable
+    sqlite3_stmt *stmt;
 
-    strcpy(word, ""); // Extra stuff needed for a sql statement
+    char *sql = "INSERT INTO katakana_mode (Katakana, Romaji, English) VALUES (?, ?, ?);";
 
-    strcpy(sql, word); // Create the completed sql statement
-
-    if (i % 2 == 0)
+    FILE *in_file;
+    in_file = fopen(filename, "r");
+    if (in_file == NULL)
     {
-        sqlite3.exec() // Insert to the hirigana portion of the database
+        printf("Filename: %s\n", filename);
+        printf("error opening file\n");
+        ret_val = -1;
     }
-    else
+
+    exit = sqlite3_open(db_name, &db);
+
+
+    if (exit)
     {
-        sqlite3.exec() // Insert to the english portion of the database
+        ret_val = -1;
     }
+
+    if (ret_val == 0)
+    {
+        exit = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+        if (exit != SQLITE_OK)
+        {
+            printf("error preparing\n");
+            sqlite3_close(db);
+            ret_val = -1;
+        }
+
+        while (ret_val != -1)
+        {
+            if (fgets(katakana, 50, in_file) == NULL) 
+            {
+                break;
+            }
+            if (fgets(romaji, 50, in_file) == NULL) 
+            {
+                break;
+            }
+            if (fgets(english, 50, in_file) == NULL) 
+            {
+                break;
+            }
+
+            sqlite3_bind_text(stmt, 1, katakana, -1, SQLITE_STATIC);
+            sqlite3_bind_text(stmt, 2, romaji, -1, SQLITE_STATIC);
+            sqlite3_bind_text(stmt, 3, english, -1, SQLITE_STATIC);
+
+            exit = sqlite3_step(stmt);
+            if (exit != SQLITE_DONE)
+            {
+                fprintf(stderr, "Execution failed: %s\n", sqlite3_errmsg(db));
+                sqlite3_close(db);
+                ret_val = -1;
+            }   
+
+            sqlite3_reset(stmt);
+            sqlite3_clear_bindings(stmt);
+        }
+        sqlite3_finalize(stmt);
+    }
+
+    if (ret_val == 0)
+    {
+        sqlite3_close(db);
+        fclose(in_file);
+    }
+
+    return ret_val;
 }
-
-*/
